da7ccdb264bafac010bd6fb3230207f7f38f06ec
George Mironov <kroid@yandex.ru>
2021-06-06 19:20:37 +0300

    hotfix

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import { base } from '$app/paths';
+
+	let prefix = base;
+
+	if (prefix && prefix[prefix.length - 1] == '/') {
+		prefix = prefix.substring(0, prefix.length - 1);
+	}
 </script>
 
 <svelte:head>
@@ -9,7 +15,7 @@
 <div class="container" style="margin-bottom: 20px;">
 	<div class="row">
 		<div class="col">
-			<a href={base}>Home</a>
+			<a href="{prefix}/">Home</a>
 		</div>
 	</div>
 </div>
